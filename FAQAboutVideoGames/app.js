0f78ef9d613104f674a01e9412d9ad9906d73357
Alkın Korkut <78097693+alkinkorkut@users.noreply.github.com>
2023-02-25 14:53:53 +0300

    last commit

--- FAQAboutVideoGames/app.js
+++ FAQAboutVideoGames/app.js
@@ -1,3 +1,9 @@
 const questions = document.querySelectorAll(".question-container");
 
 console.log(questions);
+
+questions.forEach(question => {
+    question.addEventListener("click", function(){
+        question.classList.toggle("active");
+    })
+});
